6de1d7021f2ba685f8e94f7a4990ce2ea83d2fe4
956159241 <956159241@qq.com>
2017-04-13 09:20:02 +0800

    4.13登录完成

---
+++ Iotzzh.CMS/Iotzzh.CMS.DAL/SqlHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Iotzzh.CMS.DAL
+{
+    //数据处理采用的ado.net
+    public class SqlHelper
+    {
+        private static readonly string connString = ConfigurationManager.ConnectionStrings
+            ["connStr"].ConnectionString;
+        //添加一个查询
+        public static DataTable GetTable(string sql, CommandType type, params SqlParameter[] pars)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlDataAdapter apter = new SqlDataAdapter(sql, conn))
+                {
+                    apter.SelectCommand.CommandType = type;
+                    if (pars != null)
+                    {
+                        apter.SelectCommand.Parameters.AddRange(pars);
+                    }
+                    DataTable da = new DataTable();
+                    apter.Fill(da);
+                    return da;
+                }
+            }
+        }
+        public static int ExecuteNonquery(string sql,CommandType type,params SqlParameter[] pars)
+        {
+            using(SqlConnection conn = new SqlConnection(connString))
+            {
+                using(SqlCommand cmd = new SqlCommand(sql,conn))
+                {
+                    cmd.CommandType = type;
+                    if (pars != null)
+                    {
+                        cmd.Parameters.AddRange(pars);
+                    }
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ Iotzzh.CMS/Iotzzh.CMS.DAL/UserInfoDal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Iotzzh.CMS.Model;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Iotzzh.CMS.DAL
+{
+    public class UserInfoDal
+    {
+        public UserInfo GetUserInfo(string userName,string userPwd)
+        {
+            string sql = "select * from UserInfo where UserName = @UserName" + 
+                " and UserPwd = @UserPwd";
+            SqlParameter[] pars = {
+                                    new SqlParameter("@UserName",SqlDbType.NVarChar,50),
+                                    new SqlParameter("@UserPwd",SqlDbType.NVarChar,50)
+                                 };
+            pars[0].Value = userName;
+            pars[1].Value = userPwd;
+            DataTable da = SqlHelper.GetTable(sql, CommandType.Text, pars);
+            UserInfo userInfo = null;
+            if (da.Rows.Count > 0)
+            {
+                userInfo = new UserInfo();
+                LoadEntity(userInfo,da.Rows[0]);
+            }
+            return userInfo;
+        }
+        public void LoadEntity(UserInfo userInfo, DataRow row)
+        {
+            userInfo.Id = Convert.ToInt32(row["Id"]);
+            //判断是否为空，对于上面的Id也可以做此操作，由于设置了主键不会为空的
+            //就没有必要执行这一步了
+            userInfo.UserName = row["UserName"] != DBNull.Value?
+                row["UserName"].ToString():string.Empty;
+            userInfo.UserPwd = row["UserPwd"] != DBNull.Value ?
+                row["UserPwd"].ToString() : string.Empty;
+            userInfo.UserMail = row["UserMail"] != DBNull.Value ?
+                row["UserMail"].ToString() : string.Empty;
+            userInfo.RegTime = Convert.ToDateTime(row["RegTime"]);
+        }
+    }
+}
