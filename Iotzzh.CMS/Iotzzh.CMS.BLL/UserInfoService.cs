5c25145c102a2c369ab767697bb8537c00e00849
956159241 <956159241@qq.com>
2017-04-13 09:19:25 +0800

    4.13登录完成

---
+++ Iotzzh.CMS/Iotzzh.CMS.BLL/UserInfoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Iotzzh.CMS.Model;
+
+namespace Iotzzh.CMS.BLL
+{
+    public class UserInfoService
+    {
+        DAL.UserInfoDal UserInfoDal = new DAL.UserInfoDal();
+        public UserInfo GetUserInfo(string userName, string userPwd)
+        {
+            return UserInfoDal.GetUserInfo(userName,userPwd);
+        }
+    }
+}
