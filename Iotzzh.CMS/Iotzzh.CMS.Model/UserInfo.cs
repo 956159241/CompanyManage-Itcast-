c22a894d66c586a29db8d42c2388bd5ebea9b1fd
956159241 <956159241@qq.com>
2017-04-13 09:20:14 +0800

    4.13登录完成

---
+++ Iotzzh.CMS/Iotzzh.CMS.Model/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iotzzh.CMS.Model
+{
+    //为UserInfo表创建一个实体类
+    public class UserInfo
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserPwd { get; set; }
+        public string UserMail { get; set; }
+        public DateTime RegTime { get; set; }
+    }
+}
